f359264d0ef038870ad7e18069ad802fbe77c82c
DAVID BOLAÑOS <juan.bolanos@cimat.mx>
2019-10-18 19:19:40 -0500

    Initial working commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run('0.0.0.0', '8000', debug=True)
